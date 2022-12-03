1f739ef764f0b47504736615456ee73a3ec955ef
Matt Parker <mail@mattparker.com>
2022-12-03 09:17:48 -0500

    Update selector.sh

--- selector.sh
+++ selector.sh
@@ -3,7 +3,7 @@ mysterynumber=$(( 1 + RANDOM % 10 ))
 
 echo $mysterynumber
 
-filename=/Users/mattparker/fortunes.txt
+filename=https://raw.githubusercontent.com/maparker/quotes/main/quotes.txt
 declare -a fortunes
 IFS=$'\n'
 fortunes=(`cat "$filename"`)
