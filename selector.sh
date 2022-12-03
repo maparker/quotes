14ff69cdbf3d36e9147758f822690b92439033e4
Matt Parker <mail@mattparker.com>
2022-12-03 14:36:18 +0000

    Refined code to use the total number of quotes

--- selector.sh
+++ selector.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-mysterynumber=$(( 1 + RANDOM % 10 ))
 
-echo $mysterynumber
-
-filename=https://raw.githubusercontent.com/maparker/quotes/main/quotes.txt
+filename=quotes.txt
 declare -a fortunes
 IFS=$'\n'
 fortunes=(`cat "$filename"`)
 
+totalQuotes=$(echo "${#fortunes[@]}")
+
+mysterynumber=$(( 1 + RANDOM % ${totalQuotes} ))
+echo ${mysterynumber}
 echo "${fortunes[mysterynumber]}"
 
-echo "${#fortunes[@]}"
+
