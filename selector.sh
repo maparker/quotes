a6d349d69accfbd8e93682926132b5a6a8f19901
Matt Parker <mattparker@MP-M1.parkerhome.tech>
2022-12-04 13:32:16 -0500

    Updated final Script

--- selector.sh
+++ selector.sh
@@ -1,14 +1,36 @@
 #!/bin/bash
 
-filename=quotes.txt
-declare -a fortunes
+# Get the Twilio info
+SID=$(grep "TwilioSID" ~/.secrets | cut -d ' ' -f2)
+Token=$(grep "TwilioToken" ~/.secrets | cut -d ' ' -f2)
+
+# Change to the repo directory and update the repon from remote.
+cd /Users/mattparker/Documents/quotes/
+gh repo sync
+
+# Define quote file
+quoteFile=$(cat /Users/mattparker/Documents/quotes/quotes.txt)
+#echo $quoteFile
+
+# Setup array from quote file
+declare -a quotes
 IFS=$'\n'
-fortunes=(`cat "$filename"`)
+quotes=(`echo "$quoteFile"`)
 
-totalQuotes=$(echo "${#fortunes[@]}")
+# Count number of items in array
+totalQuotes=$(echo "${#quotes[@]}")
 
-mysterynumber=$(( 1 + RANDOM % ${totalQuotes} ))
-echo ${mysterynumber}
-echo "${fortunes[mysterynumber]}"
+# Pick a random number from the total number of lines in the quote file.
+randomNumber=$(( RANDOM % ${totalQuotes} ))
+#echo ${totalQuotes}
+#echo ${randomNumber}
 
+# Randomly pick a quote
+selectedQuote=$(echo "${quotes[randomNumber]}")
+#echo ${selectedQuote}
 
+#curl -X POST https://api.twilio.com/2010-04-01/Accounts/${SID}/Messages.json \
+#--data-urlencode "Body=${selectedQuote}" \
+#--data-urlencode "From=+18058746530" \
+#--data-urlencode "To=+17043505464" \
+#-u ${SID}:${Token}
